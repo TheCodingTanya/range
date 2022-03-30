e5a962918f6e35300baae6de728a84edca2592ba
Tanya <thetwtanya@gmail.com>
2022-03-30 13:22:30 +0800

    more range

---
+++ range2.py
@@ -0,0 +1,19 @@
+# range 範圍
+# python內建功能:清單產生器
+
+range(5) # [0, 1, 2, 3, 4]
+range(3) # [0, 1, 2]
+
+range(2, 5) # [2, 3, 4]
+range(8, 10) # [8, 9]
+range(2, 10, 3) # 2是開始 10是結尾 遞增值是3 [2, 5, 8]
+range(3, 8, 2) # [3, 5, 7]
+range(10, 3, -2) # [10, 8, 6, 4]
+
+
+# cars = ['toyota', 'honda', 'audi', 'bmw']
+# for i in range(3):
+# print('這是第', i + 1, '台: ', cars[i])
+
+# for car in cars[:3]:
+# print(car)
